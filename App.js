e3db61929aa33e699b1c7c59f57b622d996122e5
ikhlosi <105212465+ikhlosi@users.noreply.github.com>
2023-03-17 07:54:15 +0100

    FlatList component
    MyTextInput gone

--- App.js
+++ App.js
@@ -1,53 +1,34 @@
 // import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 import MyTextInput from "./src/components/MyTextInput";
 
 export default function App() {
-  // voeg 2 textinput comps en koppel ze met de juiste states
-  const [name, setName] = useState("Joris");
-  const [age, setAge] = useState("30");
-  const [text, onChangeText] = useState("");
+  const data = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+    { id: 3, name: "Bob" },
+    { id: 4, name: "Alice" },
+  ];
+  const [people, setPeople] = useState(data);
 
-  // handig op die manier om controles uit te voeren alvorens de Name te veranderen
-  const nameChangeHandler = (value) => setName(value);
+  // gebruik FlatList om data te tonen
 
   return (
     <View style={styles.container}>
-      <MyTextInput
-        caption="Name:"
-        myOnChangeText={nameChangeHandler}
-        myPlaceholder="Enter your name"
+      <FlatList
+        numColumns={2}
+        data={people}
+        renderItem={(props) => (
+          <Text style={styles.item}>{props.item.name}</Text>
+        )}
+        keyExtractor={(item) => item.id}
       />
-      <MyTextInput
-        caption="Age:"
-        myOnChangeText={(value) => setAge(value)}
-        myPlaceholder="Enter your age"
-        isNumeric={true}
-      />
-      {/* <Text> name: {name}</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your name"
-        // onChangeText={setName}
-        // better:
-        onChangeText={(value) => setName(value)}
-        keyboardType="email-address"
-      />
-      <Text> age: {age}</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your age"
-        onChangeText={setAge}
-        keyboardType="numeric"
-        secureTextEntry={true} // standaard op `true`
-      /> */}
       {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
-// TODO: styles in MyTextInput component
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -55,17 +36,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    borderRadius: 3,
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
-  inputFocus: {
-    borderWidth: 3,
-    borderColor: "red",
-    padding: 12,
+  item: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
   },
 });
